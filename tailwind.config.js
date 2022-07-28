4a64401409d7055bb999d2aa2b7a40c3240a29c6
Ángel Polo <37557672+angelpolotorres@users.noreply.github.com>
2022-07-28 13:33:52 +0200

    añadido seccion puntos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./*.html'],
+    theme: {
+        container: {
+            center: true,
+            padding: '1.25rem'
+        },
+        extend: {
+            screens: {
+                xl: '1140px'
+            }
+        }
+    },
+    plugins: []
 };
